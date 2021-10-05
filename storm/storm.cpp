f5659a5cfba2a132090890f8d723074aa925c81b
Whitchurch <neoblackcyptron@gmail.com>
2021-10-04 18:49:11 -0700

    Push in basic implementation of the Binary search tree. Also adding in post-order traversal, with book  flag to delete the visited nodes, to clear up the BST data structure.

--- storm/storm.cpp
+++ storm/storm.cpp
@@ -2,19 +2,100 @@
 //
 
 #include <iostream>
+using namespace std;
+
+//Constant definition:
+const int nullvalue = 0;
+
+//Class definitions:
+//	1. Binary Search Tree
+class treeNode
+{
+//Keeping the members public for ease of access.
+//I may move these to private and implement a getter/setter in the future to make the code cleaner.
+public: 
+	int nodeKey;
+	treeNode *leftNode;
+	treeNode *rightNode;
+
+public: treeNode(int a)
+	{
+		this->nodeKey = a;
+		this->leftNode = nullvalue;
+		this->rightNode = nullvalue;
+	}
+
+	//Common shared by all the treeNode objects
+	static treeNode* buildBinarySearchTree(treeNode *t1, treeNode *root)
+	{
+		if (root == nullvalue)
+		{
+			root = t1;
+		}
+		else
+		{
+			if (t1->nodeKey > root->nodeKey)
+			{
+				root->rightNode = buildBinarySearchTree(t1, root->rightNode);
+			}
+			else if (t1->nodeKey < root->nodeKey)
+			{
+				root->leftNode = buildBinarySearchTree(t1, root->leftNode);
+			}
+			else if (t1->nodeKey == root->nodeKey)
+			{
+				root->leftNode = buildBinarySearchTree(t1, root->leftNode);
+			}
+		}
+
+		return root;
+	}
+
+	static void postOrderTraversalDelete(treeNode *root,bool deleteTree) //Can also be used to delete the contents of the tree and free up the memory
+	{
+		if (root == nullvalue)
+		{
+			//cout << root->nodeKey << endl;
+			return;
+		}
+		else
+		{
+			postOrderTraversalDelete(root->leftNode,deleteTree);
+			postOrderTraversalDelete(root->rightNode,deleteTree);
+			if (deleteTree == true)
+			{
+				//cout << root->nodeKey << endl;
+				free(root);
+			}
+			else
+			{
+				cout << root->nodeKey << endl;
+			}
+			
+		}
+	}
+};
+
+//	2. Max-Heap:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//int a[] = { 1,5,11,4,6,9,8};
+	//int a[] = { 1,2,3,4,5 };
+	//int a[] = { 8,3,10,1,6,14,4,7,13 };
+	//int a[] = { 4,12,10,18,24,22,15,31,44,35,66,90,70,50,25 };
+	int a[] = { 25,15,50,10,22,35,70,4,12,18,24,31,44,66,90 };
+	treeNode *root = nullvalue; //set root of BST to null.
+	
+	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	{
+		treeNode *newNode = new treeNode(a[i]);
+		root = root->buildBinarySearchTree(newNode,root);
+	}
+
+	root->postOrderTraversal(root,true);
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
