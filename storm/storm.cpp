8cfc7e61805e060c258b68604004645c40ea6b16
Whitchurch <neoblackcyptron@gmail.com>
2021-10-06 00:13:23 -0700

    Created a basic hash table, It does basic insert, but still needs a provision to do the inserts at the same key level, without overwrites

--- storm/storm.cpp
+++ storm/storm.cpp
@@ -144,112 +144,112 @@ int* buildMaxHeap(int inputArray[], int heapSize)
 	return inputArray;
 }
 
+// 3. Hash Table basic implementation:
 
-//class heapNode
-//{
-//public:
-//	int nodeKey;
-//	heapNode *leftNode;
-//	heapNode *rightNode;
-//private:
-//	static int returnLeft(int currentIndex)
-//	{
-//		return((2 * currentIndex) + 1); // return (2i) location, we add + 1 since we start at 0 index.
-//	}
-//
-//	static int returnRight(int currentIndex)
-//	{
-//		return((2 * currentIndex) + 2); //return (2i+1) location, we add + 1 since we start at 0 index.
-//	}
-//
-//public:
-//	static heapNode* maxHeapify(int inputArray[], int violatingNodeIndex, int heapSize)
-//	{
-//		heapNode *result;
-//		//Get the left and right index values, below the violating node.
-//		int leftIndex = returnLeft(violatingNodeIndex);
-//		int rightIndex = returnRight(violatingNodeIndex);
-//		int largest = 0; // setting the largest to 0, this needs to be run to make sure, setting to 0 does not break the code.
-//		int tempStorage = 0;
-//
-//		//Decide if the leftIndex has the larger element.
-//		if (leftIndex <= heapSize && inputArray[leftIndex] > inputArray[violatingNodeIndex])
-//		{
-//			largest = leftIndex;
-//		}
-//		else
-//		{
-//			largest = violatingNodeIndex;
-//		}
-//
-//		//Decide if the rightIndex has the larger element.
-//		if (rightIndex <= heapSize && inputArray[rightIndex] > inputArray[largest])
-//		{
-//			largest = rightIndex;
-//		}
-//		
-//		//Only make the swap, it either the left or right indexes are bigger than the violating node index.
-//		//This information has already been tracked in the larges variable:
-//		if (largest != violatingNodeIndex)
-//		{
-//			tempStorage = inputArray[violatingNodeIndex];
-//			inputArray[violatingNodeIndex] = inputArray[largest];
-//			inputArray[largest] = tempStorage;
-//		}
-//
-//		//Once the swap is done, we need to recurse down the heap, to ensure, lower sub-trees are still heaps.
-//		result = maxHeapify(inputArray, largest, heapSize);
-//
-//		return result;
-//	}
-//
-//	static heapNode* buildMaxHeap(int inputArray[], int heapSize)
-//	{
-//		//Start at the first node, which is not a max-heap, to begin the max-heapify.
-//		//Work your way from this point all the way to the root node, all the while
-//		//running max-heapify on every node visited.
-//		for (int violatingNodeIndex = heapSize / 2; violatingNodeIndex > 0; violatingNodeIndex--)
-//		{
-//			maxHeapify(inputArray, violatingNodeIndex, heapSize);
-//		}
-//
-//		return inputArray;
-//	}
-//	
-//};
+//Create the linkedlist node
+class nodeForLinkedList
+{
+public:
+	int key;
+	nodeForLinkedList *nextNodeAddress;
+
+public: 
+	nodeForLinkedList()
+	{
+
+	}
+	nodeForLinkedList(int key)
+	{
+		this->key = key;
+		this->nextNodeAddress = nullvalue;
+	}
+};
+
+class HashTable
+{
+public:
+	nodeForLinkedList *headptr;
+	int sizeofTable;
+public: 
+	HashTable(int sizeofTable)
+	{
+		this->sizeofTable = sizeofTable;
+	}
+public:
+	//Function to build HashTable
+	 nodeForLinkedList* buildHashTable()
+	{
+		
+		//Create an array of pointers. Each pointer can point to a nodeForLinkedListObject;
+		nodeForLinkedList *ptr = new nodeForLinkedList[this->sizeofTable];
+		return ptr;
+	}
+
+	//Function for hashing the value to a location in the hashTable
+	int hashTheValue(int value)
+	{
+		return(value%this->sizeofTable);
+	}
+
+	//Function Insert items into the hash table
+	void insertKey(int key)
+	{
+		int index = this->hashTheValue(key);
+		this->headptr[index] = nodeForLinkedList(key);
+	}
+};
+
 
 int main()
 {
-	//int a[] = { 1,5,11,4,6,9,8};
-	int a[] = { 1,2,3,4,5 };
-	//int a[] = { 8,3,10,1,6,14,4,7,13 };
-	//int a[] = { 4,12,10,18,24,22,15,31,44,35,66,90,70,50,25 };
-	//int a[] = { 25,15,50,10,22,35,70,4,12,18,24,31,44,66,90 };
-	treeNode *root = nullvalue; //set root of BST to null.
+	////int a[] = { 1,5,11,4,6,9,8};
+	//int a[] = { 1,2,3,4,5 };
+	////int a[] = { 8,3,10,1,6,14,4,7,13 };
+	////int a[] = { 4,12,10,18,24,22,15,31,44,35,66,90,70,50,25 };
+	////int a[] = { 25,15,50,10,22,35,70,4,12,18,24,31,44,66,90 };
+	//treeNode *root = nullvalue; //set root of BST to null.
+	//
+	////Building a BST
+	//for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	//{
+	//	treeNode *newNode = new treeNode(a[i]);
+	//	root = root->buildBinarySearchTree(newNode,root);
+	//}
+
+	////Post-order Traversal of the tree,
+	//root->postOrderTraversalDelete(root,true);
+
+
+	////MaxHeapify trial
+	//int a_1[] = { 1,2,3,4,5 };
+	//int arrayLength = sizeof(a_1) / sizeof(a_1[0]);
+	//int *result;
+	//result = buildMaxHeap(a_1, arrayLength );
+
+	////View the max-heap:
+	//for (int i = 0; i < arrayLength; i++)
+	//{
+	//	cout << a_1[i] << endl;
+	//}
+	//
+
+	//Create an instance of the hash table.
+	HashTable* h1 = new HashTable(8);
 	
-	//Building a BST
-	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	//Build the array of node pointers inside the hash table. 
+	h1->headptr = h1->buildHashTable();
+	h1->insertKey(15);
+	h1->insertKey(5);
+	h1->insertKey(1);
+	h1->insertKey(17);
+
+	for (int i = 0; i < 8; i++)
 	{
-		treeNode *newNode = new treeNode(a[i]);
-		root = root->buildBinarySearchTree(newNode,root);
-	}
-
-	//Post-order Traversal of the tree,
-	root->postOrderTraversalDelete(root,true);
-
-
-	//MaxHeapify trial
-	int a_1[] = { 1,2,3,4,5 };
-	int arrayLength = sizeof(a_1) / sizeof(a_1[0]);
-	int *result;
-	result = buildMaxHeap(a_1, arrayLength );
-
-	//View the max-heap:
-	for (int i = 0; i < arrayLength; i++)
-	{
-		cout << a_1[i] << endl;
+		cout << h1->headptr[i].key << endl;
 	}
 	
+
+
 
 }
 
